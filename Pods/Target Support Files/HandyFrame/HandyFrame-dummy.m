833719b087c483fdb7b99506ba97a6b51e40701a
Star <teaker@Teaker-deMacBook-Pro.local>
2018-04-06 22:31:26 +0800

    1.0

---
+++ Pods/Target Support Files/HandyFrame/HandyFrame-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_HandyFrame : NSObject
+@end
+@implementation PodsDummy_HandyFrame
+@end
---
+++ Pods/Target Support Files/Pods-DDSoundWave/Pods-DDSoundWave-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DDSoundWave : NSObject
+@end
+@implementation PodsDummy_Pods_DDSoundWave
+@end
